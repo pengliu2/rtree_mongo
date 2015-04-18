e5b8ce6e52442968c65d4050af0f1bf323b5132e
Peng Liu <a22543@motorola.com>
2015-04-17 23:22:15 -0500

    check in Matt's compilable copy
    Signed-off-by: Peng Liu <a22543@motorola.com>

---
+++ src/mongo/db/rtree/Entry.cpp
@@ -0,0 +1,72 @@
+/*
+ * Entry.cpp
+ *
+ *  Created on: Apr 12, 2015
+ *      Author: assma
+ */
+
+#include "Node.h"
+#include <iostream>
+#include <unordered_map>
+#include "BoundingBox.h"
+#include "Entry.h"
+
+Entry::Entry(BoundingBox i, Node c){
+	I = i;
+	childNode = c;
+	//childDoc = NULL;
+}
+Entry::Entry(BoundingBox i, std::unordered_map<int, std::string> c){
+	I = i;
+	childDoc = c;
+}
+Node Entry::getChildPointer(){ return childNode; }
+std::unordered_map<int, std::string> Entry::getTupleID(){ return childDoc; }
+BoundingBox Entry::getI() const { return I; }
+int Entry::getNumDimen(){ return I.getNumDimen(); }
+void Entry::setChild(Node c){ childNode = c; }
+void Entry::setChild(std::unordered_map<int, std::string> h){ childDoc = h; }
+void Entry::setI(BoundingBox i){ I = i; }
+bool operator==(const Entry& lhs, const Entry& rhs) { 
+	return lhs.equal(rhs);
+}
+bool Entry::equal(const Entry other) const {
+	if (I.equal(other.getI()) && childNode.equal(other.childNode) && childDoc == other.childDoc){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+//using namespace std;
+/*
+Entry::Entry(){}
+
+Entry::Entry(BoundingBox i, Node c){
+	I = i;
+	childNode = c;
+	//childDoc = NULL;
+}
+Entry::Entry(BoundingBox i, std::unordered_map<int, std::string> c){
+	I = i;
+	//childNode = NULL;
+	childDoc = c;
+}
+Node Entry::getChildPointer(){ return childNode;}
+std::unordered_map<int, std::string> Entry::getTupleID(){ return childDoc; }
+BoundingBox Entry::getI(){return I;}
+int Entry::getNumDimen(){return I.getNumDimen();}
+void Entry::setChild(Node c){childNode = c;}
+void Entry::setChild(std::unordered_map<int, std::string> h){ childDoc = h; }
+void Entry::setI(BoundingBox i){I=i;}
+bool Entry::equal(Entry other){
+	if(I.equal(other.getI())&&childNode.equal(other.childNode)&&childDoc==other.childDoc){
+		return true;
+	}else{
+		return false;
+	}
+}
+*/
+
+
